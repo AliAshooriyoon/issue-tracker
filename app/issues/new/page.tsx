a29306564a9360d7f0d851c454746013ad8878f0
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-09-22 22:32:15 +0330

    some changes

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,31 @@
-import { TextArea, TextField } from "@radix-ui/themes";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+"use client"
+import { Button, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+interface NewIssueType {
+  title: string;
+  description: string;
+}
 const NewIssue = () => {
+  const sendData = async (d: { title: string, description: string }) => {
+    const res = await fetch("/api/issues", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" }
+      , body: JSON.stringify(d)
+    })
+    const data = await res.json()
+    console.log(data)
+  }
   return <>
-    <div className="space-y-4">
-
-      <TextField.Root placeholder="Title">
+    <form className="space-y-4 max-w-96 mx-auto" >
+      <TextField.Root className="" placeholder="Title">
         <TextField.Slot>
           {/* <MagnifyingGlassIcon height="16" width="16" /> */}
         </TextField.Slot>
       </TextField.Root>
-
-      <TextArea placeholder="Descriotion" />
-    </div>
+      <SimpleMDE className="color-white bg-white" />
+      <Button>Create Issue</Button>
+    </form>
   </>
 }
 export default NewIssue;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@radix-ui/themes"
 import Link from "next/link";
 const Issues = () => {
   return <>
-    <div className='bg-stone-800'>
+    <div className=''>
 
       <Button>
         <Link href={'issues/new'}> new Issue</Link>
