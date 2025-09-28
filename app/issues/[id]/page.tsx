c7f6df13f98f2a3fd64b6d9a33b46f0df7ba19fb
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-09-28 18:49:36 +0330

    change some options in issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,40 +1,30 @@
 import { ChangeIssue } from "@/app/components/ChangeIssue";
-import { DataType } from "@/app/components/ShowIssues";
 import prisma from "@/prisma/client";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 interface IssuePageProps {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }
 
 const Issue = async ({ params }: IssuePageProps) => {
-  const res = await fetch("http://localhost:3000/api/issues", {
-    cache: "no-store",
-  });
-  if (!res.ok) {
-    throw new Error("Connection failed!");
-  }
-
-  const data = await res.json();
-
-  const slug: string = params.id; // Issue Name + Issue ID
+  const resolvedParams = await params;
+  const slug: string = resolvedParams.id; // Issue Name + Issue ID
   const slugArray = slug.split("-");
-
   const issueId: number = Number(slugArray[slugArray.length - 1]);
 
-  const valid = data.find((e: DataType) => e.slug === slug);
-  if (!valid) {
+  if (Number.isNaN(issueId)) {
     notFound();
   }
 
   const issue = await prisma.issue.findUnique({
-    where: {
-      id: issueId,
-    },
+    where: { id: issueId },
   });
+
+  if (!issue) {
+    notFound();
+  }
+
 
   return <>{issue && <ChangeIssue issueData={issue} />}</>;
 };
