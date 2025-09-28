129d04fe4d6fbc9692e4e92655dd3d974bbed89f
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-09-28 18:26:00 +0330

    resolve a bug in issue page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,47 +3,45 @@ import { DataType } from "@/app/components/ShowIssues";
 import prisma from "@/prisma/client";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
-const Issue = async ({ params }: { params: { id: string } }) => {
-  const res = await fetch("/api/issues")
-  if (!res.ok) {
-    throw new Error("Connection faild!");
-  }
-  const data = await res.json();
-  const slug: string = params.id;  // Issue Name + Issue ID;
-  const slugArray = slug.split('-')
 
-  const issueId: number = Number(slugArray[slugArray.length - 1])
-  const valid = data.find((e: DataType) => {
-    return e.slug == slug
-  })
-  if (!valid) {
-    notFound()
+interface IssuePageProps {
+  params: {
+    id: string;
+  };
+}
+
+const Issue = async ({ params }: IssuePageProps) => {
+  const res = await fetch("http://localhost:3000/api/issues", {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Connection failed!");
   }
+
+  const data = await res.json();
+
+  const slug: string = params.id; // Issue Name + Issue ID
+  const slugArray = slug.split("-");
+
+  const issueId: number = Number(slugArray[slugArray.length - 1]);
+
+  const valid = data.find((e: DataType) => e.slug === slug);
+  if (!valid) {
+    notFound();
+  }
+
   const issue = await prisma.issue.findUnique({
     where: {
-      id: issueId
-    }
-  })
+      id: issueId,
+    },
+  });
 
-
-  //----------- Space ------------------
-  // Edit Issue data
-  // Delete Button
-
-  const removeAction = (idToRemove: number) => {
-    console.log(idToRemove)
-  }
-
-
-  return <>
-    {issue && <ChangeIssue issueData={issue} />}
-  </>
-}
+  return <>{issue && <ChangeIssue issueData={issue} />}</>;
+};
 
 export const metadata: Metadata = {
   title: "Issue Tracker",
-  description: "Issue Tracker project"
+  description: "Issue Tracker project",
 };
-
 
 export default Issue;
