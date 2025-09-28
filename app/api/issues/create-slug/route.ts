f9061e12d4140da385468f5265a751ad91341912
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-09-28 15:13:59 +0330

    design filter UI

--- app/api/issues/create-slug/route.ts
+++ app/api/issues/create-slug/route.ts
@@ -4,7 +4,9 @@ import { NextResponse } from 'next/server';
 export const PATCH = async () => {
   const issues = await prisma.issue.findMany();
   for (const item of issues) {
-    const slug = `${item.title.trim().split(' ').join('').toLowerCase()}-${item.id}`;
+    const cleanItem = item.title.replace(/[^\w\s]/g, '');
+
+    const slug = `${cleanItem.trim().split(' ').join('').toLowerCase()}-${item.id}`;
     await prisma.issue.update({
       where: { id: item.id },
       data: {
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,15 @@
-
 import { Button } from "@radix-ui/themes"
 import Link from "next/link";
 import ShowIssues from "../components/ShowIssues";
-const Issues = () => {
+const Issues = async ({ searchParams }:
+  { searchParams: { [key: string]: string | undefined | string[] } }
 
+) => {
+  const statusParam = searchParams.status
+  console.log(searchParams.status)
   return <>
-    <div className=''>
-      <ShowIssues />
+    <div className='mb-28 text-left'>
+      <ShowIssues status={statusParam} />
       <Button>
         <Link href={'issues/new'}> new Issue</Link>
       </Button>
